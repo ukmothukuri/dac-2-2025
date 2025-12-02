b6348fb9b23b221fbf17d07724a90f0af264f15e
ukmothukuri <ukmothukuri@gmail.com>
2025-12-02 16:50:46 +0530

    Updated the HibernateUtil

--- ecommerceapp/src/main/java/in/com/entity/HibernateUtil.java
+++ ecommerceapp/src/main/java/in/com/entity/HibernateUtil.java
@@ -3,17 +3,23 @@ package in.com.entity;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+
 public class HibernateUtil {
 
 	public static SessionFactory sfactory;
 	
-	public static SessionFactory getSessionFactory() 
-	{
-		try {
-			sfactory = new Configuration().buildSessionFactory();
-		}catch(Exception ee) {
-			ee.printStackTrace();
-		}
-		return sfactory;
-	}
+	static {
+        try {
+        	// loads hibernate.cfg.xml
+            Configuration configuration = new Configuration().configure(); 
+            sfactory = configuration.buildSessionFactory();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ExceptionInInitializerError("Initial SessionFactory creation failed: " + ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sfactory;
+    }
 }
